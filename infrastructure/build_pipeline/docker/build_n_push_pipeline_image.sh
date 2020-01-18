b63067297a397f83dbe0d18ac2a4b443ff783e95
Nathan Menge <nathan.menge@gmail.com>
2020-01-17 22:45:59 -0500

    [Infrastructure] Updated support script to handle semantic version updates for me.
    Also, enhanced README with content about the CI pipeline.

--- infrastructure/build_pipeline/docker/build_n_push_pipeline_image.sh
+++ infrastructure/build_pipeline/docker/build_n_push_pipeline_image.sh
@@ -6,6 +6,13 @@ version=$(cat $version_path)
 ecr_image_prefix="347157418948.dkr.ecr.us-east-2.amazonaws.com"
 image_name="tradewinds/codebuildimage"
 export AWS_PROFILE="hashhouseremoteadmin"
+
+if command -v semver &> /dev/null
+then
+  version=$(semver bump minor $version)
+  echo $version > $version_path
+fi
+
 docker_login_cmd=$(aws ecr get-login --region "us-east-2" --no-include-email)
 eval $docker_login_cmd
 docker build -t $image_name:$version -t $ecr_image_prefix/$image_name:$version -f $pipeline_path/Dockerfile .
