a2538a02e41a751042369cf259458094bdf03406
Nathan Menge <nathan.menge@gmail.com>
2020-05-10 07:55:16 -0400

    Renamed crypto.ex to kennel_id.ex to better reflect its role.

--- lib/tradewinds/crypto/kennel_id.ex
+++ lib/tradewinds/crypto/kennel_id.ex
@@ -5,8 +5,7 @@ defmodule Tradewinds.Crypto.KennelId do
   This ID is ONLY intended to be used at the tail end of the Kennel geostring,
 which itself narrows the range of an ID to a very specific Geographical region.
 
-  There will be issues should there ever be more than a million trails in a single geographic region.
-  However, that would require a trail every day for 2739 years to happen.  WHat does the world even look like then?
+  There will be issues should there ever be more than ten thousand kennels in a single geographic region.
 """
-  use Puid, total: 1000000, risk: 1.0e8, charset: :alphanum
+  use Puid, total: 1_000, risk: 1.0e8, charset: :alphanum
 end
